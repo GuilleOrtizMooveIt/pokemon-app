036f6f80b3af35481f6631eeaef337d633f93a74
guillermoortiz <guillermo.ortiz@mooveit.com>
2026-06-19 09:32:52 -0300

    added description to hook v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import usePokemon from './hooks/usePokemon';
 
+
 function App() {
 
-    const [name, setName] = useState('pikachu');
-    const { pokemon, fetchPokemon } = usePokemon(name);
+    const [name, setName] = useState('charmander');
+    const { pokemon, description, loading, error, fetchPokemon } = usePokemon(name);
 
     useEffect(() => {
       fetchPokemon();
@@ -12,8 +13,11 @@ function App() {
 
     return (
       <div>
+        {loading && <p> Cargando...</p>}
+        {error && <p> {error}</p>}
     
         {pokemon && <img src={pokemon.sprites.front_default} />}
+        {description && <p>{description}</p>}
         
       </div>
   );
--- src/hooks/usePokemon.ts
+++ src/hooks/usePokemon.ts
@@ -1,18 +1,49 @@
 import { useState } from 'react';
-import { Pokemon } from '../PokemonType';
+import { Pokemon, PokemonSpecies } from '../PokemonType';
+import { useCallback } from 'react';
 
 function usePokemon(name: string) {
   const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+  const [description, setDescription] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
-  const fetchPokemon = async () => {
+  const fetchPokemon = useCallback( async () => {
+  	setLoading(true);
+    setError(null);
     try {
-      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-      const data = await res.json();
-      setPokemon(data);
-    } catch (err) {} 
-  };
 
-  return { pokemon, fetchPokemon };
+       const [pokemonRes, speciesRes] = await Promise.all([
+        fetch(`https://pokeapi.co/api/v2/pokemon/${name}`),
+        fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`)
+      ]);
+
+      if (!pokemonRes.ok || !speciesRes.ok) {
+        throw new Error('Pokemon no encontrado');
+      }
+
+      const pokemonData: Pokemon = await pokemonRes.json();
+      const speciesData: PokemonSpecies = await speciesRes.json();
+
+      const spanishData = speciesData.flavor_text_entries.find(
+          (entry) => entry.language.name === 'es'
+       );
+
+      setPokemon(pokemonData);
+      setDescription(spanishData ? spanishData.flavor_text : null);
+
+    } catch (err) {
+
+    	setError('Error en la busqueda: ' + err);
+
+    } finally {
+
+    	setLoading(false);
+    	
+    }
+  }, [name]);
+
+  return { pokemon, description, loading, error, fetchPokemon };
 }
 
 export default usePokemon;
